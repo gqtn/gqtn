345fb2a1cfddbe508eb1bc80477d6d8d2cee760d
gqtn <gquintana-mail@proton.me>
2025-03-11 16:54:15 -0300

    from gitlab

---
+++ tricks/gnome/purge-unused.sh
@@ -0,0 +1,32 @@
+## Clean GNOME:
+
+		sudo apt purge -y gnome-games \
+		cheese \
+		iagno lightsoff \
+		four-in-a-row \
+		gnome-robots \
+		pegsolitaire \
+		gnome-2048 \
+		hitori \
+		gnome-klotski \
+		gnome-mines \
+		gnome-mahjongg \
+		gnome-sudoku \
+		quadrapassel \
+		swell-foop \
+		gnome-tetravex \
+		gnome-taquin aisleriot \
+		libreoffice* \
+		gnome-maps \
+		gnome-music \
+		gnome-contacts \
+		evolution \
+		rhythmbox \
+		shotwell \
+		gnome-todo \
+                totem \
+                seahorse \
+                yelp 
+		sudo apt autopurge -y
+		sudo apt autoremove -y
+		sudo apt autoclean -y
