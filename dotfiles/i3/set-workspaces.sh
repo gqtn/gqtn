9c03bf524388422475590bfde78040de27283978
gqtn <gquintana-mail@proton.me>
2025-03-21 09:53:27 -0300

    My Workspaces

---
+++ dotfiles/i3/set-workspaces.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Assign workspaces to HDMI (up)
+i3-msg "workspace 1; move workspace to output HDMI-A-0"
+
+# Assign workspaces to Notebook (down)
+i3-msg "workspace 8; move workspace to output eDP"
+
+feh --bg-scale $HOME/.config/i3/default.jpg 
